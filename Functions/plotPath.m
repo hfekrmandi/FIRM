d4826d86f2fb70da0bb4c2fcec44df139b83748a
bencolvin23 <67249513+bencolvin23@users.noreply.github.com>
2020-09-25 10:57:30 -0600

    Update plotPath.m
    added comments

--- Functions/plotPath.m
+++ Functions/plotPath.m
@@ -1,15 +1,19 @@
 function plotPath(x,y,z,waypoints)
-% plotPath.m     e.anderlini@ucl.ac.uk     15/09/2017
+% plotPath.m    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This function is used to plot the path of the ROV.
+% This function is used to plot the path of the UUV
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+% Generate figure
 figure;
+% Title figure
 title('Path')
+% Create 3d plot
 plot3(x,y,z);
 hold on;
+% Add waypoints as circles to plot
 scatter3(waypoints(:,1),waypoints(:,2),waypoints(:,3),'MarkerEdgeColor',...
     [0.8500,0.3250,0.0980]);
+% Label axis
 xlabel('$x$ (m)','Interpreter','Latex');
 ylabel('$y$ (m)','Interpreter','Latex');
 zlabel('$z$ (m)','Interpreter','Latex');
